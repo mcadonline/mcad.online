afa2d89b04bab711d155682c06ed2229e5c21211
Lafe Smith <lsmith257@mcad.edu>
2024-12-16 15:01:03 -0600

    Add files via upload
    Add Fall 2024 student projects

---
+++ src/data/projects-2024FA.js
@@ -0,0 +1,33 @@
+const projects2024Fa = [
+    {
+      title: "Make it accesible",
+      by: "Lauren Pritchard",
+      description: " A digital hub of design guidelines, best practices, and resources aimed at helping designers create more accessible work.",
+      url: "https://laurpritch07.github.io/make-it-accessible/about.html",
+      img: "img/magwd/2024FA/pritchard.png",
+    },
+    {
+      by: "Brian Rogers",
+      title: "Encryptids",
+      description: "Mythical characters highlight AI threats to creatives through their misadventures.",
+      url: "https://brianrog9.github.io/encryptids/",
+      img: "img/magwd/2024FA/rogers.png",
+    },
+    {
+      by: "Sydni Rose",
+      title: "BookMark",
+      description: "A digital notebook for book notes.",
+      url: "https://sydnirose.github.io/BookMark/",
+      img: "img/magwd/2024FA/rose.png",
+    },
+    {
+      by: "Brett Smith",
+      title: "Natural Neighborhood Network",
+      description: "A unifying graphic identity to amplify the impact of local sustainable gardening.",
+      url: "https://mrbrettsmith.github.io/Project-Page/",
+      img: "img/magwd/2024FA/smith.png",
+    }, 
+  ];
+  
+  export default projects2024Fa;
+  
