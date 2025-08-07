81c914ecc0d8f33e960ebbe2a242fb5f312802f1
Lafe Smith <lsmith257@mcad.edu>
2025-08-07 12:05:49 -0500

    Add Summer 2025 projects
    Added Summer 2025 projects

---
+++ src/data/projects-2025SU.js
@@ -0,0 +1,61 @@
+const projects2025Su = [
+    {
+      title: "We See You",
+      by: "Cornel Beard",
+      description: "A creative platform and community experience designed to honor, uplift, and make visible the lived experiences of Black veterans.",
+      url: "http://www.cornelbeard.work/full-capstone-project",
+      img: "img/magwd/2025SU/beard.png",
+    },
+    {
+      by: "Camille Gonzalez",
+      title: "MOSAICO",
+      description: "An open-source forum where users can learn, contribute to, and discuss the historical narratives of Latine designers.",
+      url: "https://cgonzalez622.github.io/MOSAICO/",
+      img: "img/magwd/2025SU/gonzalez.png",
+    },
+    {
+        by: "Claire Henry",
+        title: "SuperEtte",
+        description: "A Visual Brand Identity for a Community-Centered Sustainable Marketplace.",
+        url: "https://clairehenrycreative.github.io/capstonepage/index.html",
+        img: "img/magwd/2025SU/henry.png",
+      },
+      {
+        by: "Baylee Lehmann",
+        title: "The Huntington Comes Alive—Digitally",
+        description: "Creating a more dynamic interaction between the users and the garden through a digital lens.",
+        url: "https://bayleelehmann.github.io/capstonecasestudy/",
+        img: "img/magwd/2025SU/lehmann.png",
+      },
+      {
+        by: "Devin Moody",
+        title: "The Deer Creek High School: Where Legacy Begins",
+        description: "Designing a brand to embody tradition, purpose, and pride for Deer Creek High School's historic opening.",
+        url: "https://dfmoody.github.io/capstone/index.html",
+        img: "img/magwd/2025SU/moody.png",
+      },
+      {
+        by: "Emily Morrison Weeks",
+        title: "ArtWalk",
+        description: "Designing connection through an arts and community app.",
+        url: "https://emorrisonweeks.github.io/artwalk/",
+        img: "img/magwd/2025SU/morrisonweeks.png",
+      },
+      {
+        by: "Shaun Ritchie",
+        title: "AIME",
+        description: "Managing anxiety through the power of AI.",
+        url: "https://shaundigital.github.io/Capstone2025/#about",
+        img: "img/magwd/2025SU/ritchie.png",
+      },
+      {
+        by: "Jake Zweibohmer",
+        title: "The Three Rivers Trail Wayfinding Signage System",
+        description: "Enhancomg user experience on the Three Rivers Trail in Humboldt County, Iowa through a cohesive, user friendly, and well-designed wayfinding signage system.",
+        url: "https://jakezweibohmer.github.io/capstone2025/",
+        img: "img/magwd/2025SU/zweibohmer.png",
+      },
+  ];
+  
+  export default projects2025Su;
+  
