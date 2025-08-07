66e155f0537c64a4d69f931afe438262f41d7d7c
Lafe Smith <lsmith257@mcad.edu>
2025-08-07 12:03:11 -0500

    Added projects-2025SP
    Added project for Spring 2025

---
+++ src/data/projects-2025SP.js
@@ -0,0 +1,12 @@
+const projects2025Sp = [
+    {
+      title: "Operation Inspiration",
+      by: "Kyrin Van Heise",
+      description: "A detailed examination of the cross-sections of art and design - with an open invitation to explore the world of creativity.",
+      url: "https://kvanheise.github.io/magwd-capstone/",
+      img: "img/magwd/2025SP/vanheise.png",
+    },
+  ];
+  
+  export default projects2025Sp;
+  
