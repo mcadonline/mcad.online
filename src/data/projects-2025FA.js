c959fa1004db83fb62f729d7a4e7f539f88dea2f
Lafe Smith <lsmith257@mcad.edu>
2025-12-15 12:34:48 -0600

    Add FA2025 Projects
    Added FA2025 graduate project data

---
+++ src/data/projects-2025FA.js
@@ -0,0 +1,75 @@
+const projects2025Fa = [
+    {
+      title: "Foodie Quest",
+      by: "Jennifer Breiter",
+      description: "A concept app designed to help users track, reflect on, and rediscover their favorite dining experiences through a personalized and intuitive interface.",
+      url: "https://www.jenniferbreiter.com/foodie-quest/",
+      img: "img/magwd/2025FA/breiter.png",
+    },
+    {
+      by: "Amy Freid",
+      title: "YourPath",
+      description: "Your go-to career guide, guiding you from where you are and where you want to go.",
+      url: "https://https://freidamy.github.io/capstone-page/#about/",
+      img: "img/magwd/2025FA/freid.png",
+    },
+    {
+      by: "Erin Henkel",
+      title: "Chive Blossom Creamery & Farm",
+      description: "Integrating a hand made, craft aesthetic into the visual identity design for an artisan business.",
+      url: "https://erinhenkel.github.io/capstone-project/",
+      img: "img/magwd/2025FA/henkel.png",
+    },
+    {
+      by: "Steven Martin",
+      title: "Missouri Rural Education Outreach (MOREO)",
+      description: "Breaking down barriers so rural Missouri students can reach higher education.",
+      url: "https://smartin906.github.io/capstone_page/",
+      img: "img/magwd/2025FA/martin.png",
+    }, 
+    {
+        by: "Jamie Poole",
+        title: "Golden Joy",
+        description: "An inclusive space for intergenerational connections to bloom through assistance and community service.",
+        url: "https://jamiepoole750.github.io/GoldenJoy/",
+        img: "img/magwd/2025FA/poole.png",
+      },  
+      {
+        by: "Arden Roddie",
+        title: "Sweet Little Hauntings",
+        description: "An unfiction narrative project exploring themes of grief through mystery, horror, and design.",
+        url: "https://capstone.sweetlittlehauntings.com/",
+        img: "img/magwd/2025FA/roddie.png",
+      },  
+      {
+        by: "Kallie Rollenhagen",
+        title: "Rural Design Studio",
+        description: "A design studio of and for rural communities.",
+        url: "https://ruraldesign.studio/capstone/",
+        img: "img/magwd/2025FA/rollenhagen.png",
+      },  
+      {
+        by: "Morgen Ruff",
+        title: "A New Internet Home for the Weavers Guild of Minnesota",
+        description: "A redesigned website and brand refresh for a long-running craft organization, representing the full breadth and depth of their vital work preserving and advancing the arts of weaving, spinning, and dyeing.",
+        url: "https://morgenruff.github.io/mcad-capstone-site/",
+        img: "img/magwd/2025FA/ruff.png",
+      },  
+      {
+        by: "Donchelle Santiaguin-Rollins",
+        title: "Life",
+        description: "Designing a human-like experience for mental health involving AI and real-life therapy.",
+        url: "https://donchellesantiaguin.github.io/CAPSTONE/",
+        img: "img/magwd/2025FA/santiaguin-rollins.png",
+      },  
+      {
+        by: "Josh Story",
+        title: "Nameless Faceless",
+        description: "A clothing brand that supports nonprofits by contributing sales directly to their funding efforts.",
+        url: "https://jlstory.github.io/capstone/",
+        img: "img/magwd/2025FA/story.png",
+      }, 
+  ];
+  
+  export default projects2025Fa;
+  
