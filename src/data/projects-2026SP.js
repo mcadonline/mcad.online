5d9f4441479ba58682f6d2a868574ef1a5132297
Lafe Smith <lsmith257@mcad.edu>
2026-05-14 13:31:17 -0500

    Add files via upload
    Added file for Spring 2026 capstone projects.

---
+++ src/data/projects-2026SP.js
@@ -0,0 +1,19 @@
+const projects2026Sp = [
+    {
+      title: "Skyway Navigator",
+      by: "Caitlin Jones",
+      description: "A mobile app to help you navigate the Minneapolis Skyway like a pro.",
+      url: "https://jonesing13.github.io/",
+      img: "img/magwd/2026SP/jones.png",
+    },
+    {
+        title: "Generative AI Literacy Guide for Designers",
+        by: "Luke Lundmark",
+        description: "A guide for designers and design-adjacent professionals interested in responsibly using AI technology.",
+        url: "https://llundmark.github.io/ailg.github.io/",
+        img: "img/magwd/2026SP/lundmark.png",
+      },
+  ];
+  
+  export default projects2026Sp;
+  
